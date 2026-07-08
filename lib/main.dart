9033ccf7a7ea202ababdf385ed00ae252ffb9657
Betelhem Abreham <betelhemabreham514@gmail.com>
2026-07-08 11:59:29 +0300

    added reset button

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Beti\'s First Flutter App',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -28,7 +28,9 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: .fromSeed(
+          seedColor: const Color.fromARGB(255, 58, 62, 183),
+        ),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
